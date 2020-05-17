1c611a99ecad6a5e0d777e083011d74293d8eead
marco7877 <56403434+marco7877@users.noreply.github.com>
2020-05-16 20:27:09 -0500

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ def main():
     import math
     import numpy as np
     import matplotlib.pyplot as plt
+    import pandas as pd
     def most_common(lst, n):# sirve para plotear, busca más cumun
         if len(lst) == 0: return -1
     #print(lst)
@@ -115,6 +116,7 @@ def main():
         maxi=np.max(array)
         array=(array-mini)/(maxi-mini)
         return array
+    
     def testArquitechture(SOM1,SOM2,side1,side2,sidemmr,MMR,hebbian1,hebbian2,stimuli1,stimuli2,
                           dict1rows,dict2rows,labels):
         mapping=np.empty((int(sidemmr),int(sidemmr)),dtype=object)
@@ -149,6 +151,7 @@ def main():
             y=int(result[1])
             mapping[x][y].append(labels[i])
         return mapping
+    
     def umatrix(SOM,side,plottile):
         mapping=np.zeros((int(side),int(side)),dtype=float)
         for i in range(int(side)):
@@ -186,11 +189,21 @@ def main():
                    amount+=1
                 mapping[i][j]=u/amount
         from matplotlib.pyplot import imshow, title, colorbar,show
-        imshow(mapping,cmap="binary")
+        imshow(mapping,cmap="autumn")
         title(plottile)
         colorbar()
-        show
-               
+        show()
+
+        
+    def mappingSOM(SOM,data,labels,side):
+        mapping = np.empty(shape=(int(side),int(side)), dtype=object)
+        for i in range(int(side)):
+            for j in range(int(side)):
+                mapping[i][j] = []
+        for t in range(len(data)):
+            (m_row, m_col) = closest_node(data, t, SOM, int(side), int(side))
+            mapping[m_row][m_col].append(labels[t])
+        return mapping
                     
     def plot_labelsSOM(ocurrencesMap,SOMside,imageName):
         label_map = np.zeros(shape=(int(SOMside),int(SOMside)), dtype=np.int)
@@ -202,9 +215,196 @@ def main():
         plt.colorbar()
         plt.show()
                     
-            
-            
-        
+    def plotpiechart(ocurrencesMap,side,labelslist):
+        label=np.zeros((int(side),int(side),5),dtype=int)
+        for i in range(int(side)):
+            for j in range(int(side)):
+                data=ocurrencesMap[i][j]
+                counts=np.sum(data)
+                count,uniqu=np.unique(data, return_counts=True)
+                if counts!=0:
+                    counti=uniqu/counts
+                else:
+                    counti=np.zeros(len(labelslist)+1,dtype=float)
+                    counti[0]=100
+                    uniqu=[0]
+                    count=[0]
+                array=np.zeros(5,dtype=float)
+                for e in range(len(uniqu)):
+                    indx=count[e]
+                    #print(indx)
+                    array[indx]=counti[e]
+                array=array*100
+                array.astype("int")
+                label[i][j]=array
+        c=0
+        for i in range(int(side)):
+            for j in range(int(side)):
+                c+=1
+                lislable=list(label[i][j])
+                plt.subplot(int(side),int(side),int(c),aspect = 'equal')
+                plt.tight_layout()
+                plt.axis('equal')
+                plt.pie(lislable)  
+        plt.legend( ["NaN","ba","ga","da","McGurk"],loc="best", bbox_to_anchor=(0, 0), ncol=4)
+        plt.subplots_adjust( wspace=0.001, hspace=0.001)
+        plt.show()         
+    def activationMatrix(SOM,somside,stimulidata,stimulilabels,title,plot=False):
+        activationdictionary={}
+        labelslist=list(np.unique(stimulilabels))
+        for label in labelslist:
+            mask=stimulilabels==label
+            maskeddata=stimulidata[mask]
+            counter=0
+            for stimuli in maskeddata:
+                activationmap=np.zeros((int(somside),int(somside)),dtype=float)
+                for row in range(int(somside)):
+                    for col in range(int(somside)):
+                        activationmap[row][col]=dist(SOM[row][col],stimuli)
+                activationmap=1-(normalizearray(activationmap))#inverse activation
+                if counter==0:
+                    result=activationmap
+                    counter+=1
+                else:
+                    result=activationmap+result
+            activationmap=normalizearray(activationmap)
+            if plot==True:
+                if label==1:
+                    lab="Ba"
+                elif label ==2:
+                    lab="Ga"
+                elif label==3:
+                    lab="Da"
+                elif label==4:
+                    lab="McGurk"
+                else:
+                    print("label "+str(label)+" is not recognized")
+                    lab=label
+                plt.imshow(activationmap,cmap="hot")
+                plt.tight_layout()
+                plt.colorbar()
+                plt.title(title+" activation map for "+str(lab)+" label")
+                plt.show()
+            activationmap=np.reshape(activationmap,(int(somside)*int(somside)))
+            activationdictionary[lab]=activationmap
+        activationdictionary=pd.DataFrame.from_dict(activationdictionary,orient="index")
+        if plot==True:
+            pltlabels=activationdictionary.index.values.tolist()
+            plt.plot(activationdictionary)
+            plt.ylabel("Activation per neuron")
+            plt.xlabel("Syllable")
+            plt.title(title+" activation behavior")
+            plt.show()
+            plt.imshow(activationdictionary,aspect='auto',vmin=0,vmax=1)
+            plt.title(title+" activation patterns")
+            plt.colorbar()
+            plt.yticks(np.arange(len(pltlabels)), pltlabels)
+            plt.show()
+            x=np.corrcoef(activationdictionary)
+            plt.imshow(x,cmap="plasma",vmin=-1,vmax=1)
+            for i in range(len(pltlabels)):
+                for j in range(len(pltlabels)):
+                    text = plt.text(j, i, round(x[i, j],2), ha="center", va="center", color="w")
+            plt.xticks(np.arange(len(pltlabels)), pltlabels)
+            plt.yticks(np.arange(len(pltlabels)), pltlabels)
+            plt.title(title+" activation correlations")
+            plt.colorbar()
+            plt.tight_layout()
+            plt.show()
+        path="./"+title+".csv"
+        activationdictionary.to_csv(path,index=True,header=True)
+        return activationdictionary             
+########
+    def testArquitechture2(SOM1,SOM2,side1,side2,sidemmr,MMR,hebbian1,hebbian2,stimuli1,stimuli2,
+                          dict1rows,dict2rows,labels,title,plot=True):
+        activationdictionary={}
+        labelslist=list(np.unique(labels))
+        for label in labelslist:
+            mask=labels==label
+            stimuli1masked=stimuli1[mask]
+            stimuli2masked=stimuli2[mask]
+            mapping=np.empty((int(sidemmr),int(sidemmr)),dtype=object)
+            for n in range(int(sidemmr)):
+                for m in range(int(sidemmr)):
+                    mapping[n][m]=[]
+            count=0
+            for i in range(len(stimuli1masked)):
+                winner1=winner_modalnode(stimuli1masked[i], SOM1, side1)
+                winner2=winner_modalnode(stimuli2masked[i], SOM2, side2)
+                row_1hebb=dict1rows[winner1]
+                row_2hebb=dict2rows[winner2]
+                multimodalstimuli=winnerNodesArray(winner1,winner2,side1,side2)
+                activation_map=np.zeros(shape=(int(sidemmr),int(sidemmr)),dtype=float)
+                ind1=hebbian1[row_1hebb]
+                ind2=hebbian2[row_2hebb]
+                ind=np.vstack((ind1,ind2))
+                ind=np.sum(ind,axis=0)
+                ind=normalizearray(ind)
+                counter=0
+                actmapping=np.empty((int(sidemmr),int(sidemmr)),dtype=float)
+                for r in range(int(sidemmr)):
+                    for c in range(int(sidemmr)):
+                        directactivation=dist(MMR[r][c],multimodalstimuli)
+                        actmapping[r][c]=directactivation
+                actmapping=1-(normalizearray(actmapping))
+                indirectactivation=np.reshape(ind,(int(sidemmr),int(sidemmr)))
+                activation=actmapping+indirectactivation
+                activation=normalizearray(activation)
+                if count==0:
+                    activationresult=activation
+                    count+=1
+                else:
+                    activationresult+=activation
+                result=np.where(activation==np.amax(activation))
+                x=int(result[0])
+                y=int(result[1])
+                mapping[x][y].append(label)
+            activationresult=normalizearray(activationresult)
+            if plot==True:
+                if label==1:
+                    lab="Ba"
+                elif label ==2:
+                    lab="Ga"
+                elif label==3:
+                    lab="Da"
+                elif label==4:
+                    lab="McGurk"
+                else:
+                    print("label "+str(label)+" is not recognized")
+                    lab=label
+                plt.imshow(activationresult,cmap="hot")
+                plt.tight_layout()
+                plt.colorbar()
+                plt.title(title+" activation map for "+str(lab)+" label")
+                plt.show()
+            activationdictionary[lab]=list(np.reshape(activationresult,(int(sidemmr)*int(sidemmr))))
+        activationdictionary=pd.DataFrame.from_dict(activationdictionary,orient="index")
+        if plot==True:
+            pltlabels=activationdictionary.index.values.tolist()
+            plt.plot(activationdictionary)
+            plt.ylabel("Activation per neuron")
+            plt.xlabel("Syllable")
+            plt.title(title+"activation behavior")
+            plt.show()
+            plt.imshow(activationdictionary,aspect='auto',vmin=0,vmax=1)
+            plt.yticks(np.arange(len(pltlabels)), pltlabels)
+            plt.title(title+" activation patterns")
+            plt.colorbar()
+            plt.show()
+            x=np.corrcoef(activationdictionary)
+            plt.imshow(x,cmap="plasma",vmin=-1,vmax=1)
+            for i in range(len(pltlabels)):
+                for j in range(len(pltlabels)):
+                    text = plt.text(j, i, round(x[i, j],2), ha="center", va="center", color="w")
+            plt.xticks(np.arange(len(pltlabels)), pltlabels)
+            plt.yticks(np.arange(len(pltlabels)), pltlabels)
+            plt.title(title+" activation correlations")
+            plt.colorbar()
+            plt.tight_layout()
+            plt.show()
+        path="./"+title+".csv"
+        activationdictionary.to_csv(path,index=True,header=True)
+        return mapping,activationdictionary
 
     #############################-MAIN-########################################
     #---------charging SOMS-------------------#
@@ -225,7 +425,7 @@ def main():
     sound_train= "/media/marco/MarcoHDD/github/stimuli/output_centralTendencies/output_StimulifromMultivariate/badaga_sound_train.csv"
     image_train="/media/marco/MarcoHDD/github/stimuli/output_centralTendencies/output_StimulifromMultivariate/badaga_train.csv"
     sound_test="/media/marco/MarcoHDD/github/stimuli/output_centralTendencies/output_StimulifromMultivariate/badaga_sound_test.csv"
-    image_test="/media/marco/MarcoHDD/github/stimuli/output_centralTendencies/output_StimulifromMultivariate/badaga_train.csv"
+    image_test="/media/marco/MarcoHDD/github/stimuli/output_centralTendencies/output_StimulifromMultivariate/badaga_test.csv"
     sound_traindata,sound_trainlabels=charge_files(sound_train)
     image_traindata,image_trainlabels=charge_files(image_train)
     sound_testdata,sound_testlabels=charge_files(sound_test)
@@ -237,6 +437,18 @@ def main():
                                         image_trainlabels, image_testdata, 
                                         image_testlabels, lipreading_side)
     
+    _,dict_trainSOIMA=testArquitechture2(MFCC,LipReading,mfcc_side,
+                                       lipreading_side,mmr_side,MMR,
+                                       Hebb_MFCC_MMR,
+                                       Hebb_LipRead_MMR,sound_traindata,
+                                       image_traindata,lip_mmr_rows,
+                                       mfcc_mmr_rows,image_trainlabels,"Train MMR")
+    _,dict_testSOIMA=testArquitechture2(MFCC,LipReading,mfcc_side,
+                                       lipreading_side,mmr_side,MMR,
+                                       Hebb_MFCC_MMR,
+                                       Hebb_LipRead_MMR,sound_testdata,
+                                       image_testdata,lip_mmr_rows,
+                                       mfcc_mmr_rows,image_testlabels,"Test MMR")
     TD_trainSOIMA=testArquitechture(MFCC,LipReading,mfcc_side,
                                        lipreading_side,mmr_side,MMR,
                                        Hebb_MFCC_MMR,
@@ -249,32 +461,26 @@ def main():
                                        Hebb_LipRead_MMR,sound_testdata,
                                        image_testdata,lip_mmr_rows,
                                        mfcc_mmr_rows,image_testlabels)
+    print("plotting SOM-node piechart for training")
+    plotpiechart(TD_trainSOIMA, mmr_side, [0,1,2,3])
+    plotpiechart(TD_testSOIMA, mmr_side, [0,1,2,3,4])
+    mfcc_mapping_tr=mappingSOM(MFCC, sound_traindata, sound_trainlabels, mfcc_side)
+    lipreading_mapping_tr=mappingSOM(LipReading, image_traindata,
+                                     image_trainlabels, lipreading_side)
+    plotpiechart(mfcc_mapping_tr,mfcc_side,[0,1,2,3])
+    plotpiechart(lipreading_mapping_tr, lipreading_side, [0,1,2,3,4])
+    print("plotting U-matrixes")
+    umatrix(MMR, mmr_side, "U-matrix for MMR")
+    umatrix(LipReading,lipreading_side,"U-matrix for LipReading")    
+    umatrix(MFCC,mfcc_side,"U-matrix for Syllable")
+    print("calculating activation matrixes for modal congruent stimuli")
+    sound_trainactivation=activationMatrix(MFCC,mfcc_side,sound_traindata,sound_trainlabels,"Sound train",plot=True)
+    sound_testactivation=activationMatrix(MFCC,mfcc_side,sound_testdata,sound_testlabels,"Soud test",plot=True)
+    image_trainactivation=activationMatrix(LipReading,lipreading_side,image_traindata,image_trainlabels,"Image train",plot=True)
+    image_testactivation=activationMatrix(LipReading,lipreading_side,image_testdata,image_testlabels,"Image test",plot=True)
     
-    def plotpiechart(testsoima,side,labelslist):
-        label=np.zeros((int(side),int(side),3),dtype=int)
-        for i in range(int(side)):
-            for j in range(int(side)):
-                data=testsoima[i][j]
-                counts=np.sum(data)
-                unique,count=np.unique(counts, return_counts=True)
-                if counts!=0:
-                    count=count/counts
-                else:
-                    count=np.zeros(len(labelslist),dtype=float)
-                label[i][j]=count
-        c=0
-        for i in range(int(side)):
-            for j in range(int(side)):
-                c+=1
-                lislable=list(label[])
-                plt.subplot(10,10,int(c))
-                plt.pie(label[i][j])
-                
-        #plt.legend(patches, str(labelslist), bbox_to_anchor=(0., 6.5), ncol=3)
-        #plt.savefig('resulting_images/som_iris_pies.png')
-        plt.show()
     
-
+    
 main()
 
                 
